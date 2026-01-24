f7c6ecba557cda2af078888e0a4b0aad4ab1ee93
IDGS-804-23001549 <hernandezurielcam@gmail.com>
2026-01-24 10:04:37 -0600

    formularios

---
+++ forms.py
@@ -0,0 +1,18 @@
+from wtforms import Form
+
+from wtforms import StringField, PasswordField, EmailField, BooleanField, IntegerField
+from wtforms import validators
+
+class UserForm(Form):
+    matricula = IntegerField("Matricula", [
+        validators.DataRequired(message = "El campo es requerido")
+    ])
+    nombre = StringField("Nombre", [
+        validators.DataRequired(message = "El campo es requerido")
+    ])
+    apellido = StringField("Apellido", [
+        validators.DataRequired(message = "El campo es requerido")
+    ])
+    correo = EmailField("correo", [
+        validators.Email(message = "Ingrese correo valido")
+    ])
